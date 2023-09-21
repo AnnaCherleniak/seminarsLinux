4dcbdc1aca2d59d9e9e8cb452ee70c03d4a0672b
AnnaCherleniak <114302635+AnnaCherleniak@users.noreply.github.com>
2023-09-21 19:46:51 +0700

    Create Seminar1.py

---
+++ 3247/Seminar1.py
@@ -0,0 +1,16 @@
+import subprocess
+
+
+def  command_find(command: str, text: str) -> bool:
+    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+    out = result.stdout
+    if not result.returncode:
+        my_list = out.split('\n')
+        if text in my_list:
+            return True
+        else:
+            return False
+
+
+if __name__ == '__main__':
+    print(command_find('cat /etc/os-release', 'VERSION="22.04.1 LTS (Jammy Jellyfish)"'))
