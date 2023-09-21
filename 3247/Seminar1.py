5679ce3d00c1312ddfb2a791e24df9d8ffc41217
AnnaCherleniak <114302635+AnnaCherleniak@users.noreply.github.com>
2023-09-21 19:48:25 +0700

    Update Seminar1.py

--- 3247/Seminar1.py
+++ 3247/Seminar1.py
@@ -1,7 +1,7 @@
 import subprocess
 
 
-def  command_find(command: str, text: str) -> bool:
+def command_find(command: str, text: str) -> bool:
     result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
     out = result.stdout
     if not result.returncode:
